1844a5e9a218de22cb7cf05137e03cf48cbbc2f5
ghritak <ghritakjyotikalita@gmail.com>
2024-05-02 13:05:27 +0530

    intro com

--- src/components/side/LeftIndices.js
+++ src/components/side/LeftIndices.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const LeftIndices = () => {
   return (
-    <div className='fixed h-screen justify-center items-center flex flex-col bottom-0 left-20'>
+    <div className='fixed h-screen text-white justify-center items-center flex flex-col bottom-0 left-20'>
       <div className='-rotate-90 pl-2'>3</div>
-      <div className='h-40 w-[2px] bg-gray-400 rounded-lg items-end flex'>
-        <div className='h-10 w-[2px] bg-black' />
+      <div className='h-40 w-[2px] bg-zinc-600 rounded-lg items-end flex'>
+        <div className='h-10 w-[2px] bg-white' />
       </div>
       <div className='-rotate-90 pr-2'>0</div>
     </div>
