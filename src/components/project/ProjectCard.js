24cd2f91adb77505debf8c3a6909b25195604a9b
ghritak <ghritakjyotikalita@gmail.com>
2024-05-02 17:21:19 +0530

    project

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -2,14 +2,25 @@ import React from 'react';
 import wanderway from '../../assets/project/wanderway.png';
 
 const ProjectCard = ({ item, index }) => {
+  const handleClick = () => {
+    window.open(item?.link, '_blank');
+  };
+
   return (
     <div
+      onClick={handleClick}
       className={`flex ${
         index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'
       } cursor-pointer hover:scale-110 transition-all duration-300 hover:bg-zinc-700 p-6 rounded-lg`}
     >
-      <div className={` ${index % 2 === 0 ? 'mr-10' : 'ml-10'}`}>
-        <img src={wanderway} alt='wander way' className='w-60 h-44' />
+      <div className={`${index % 2 === 0 ? 'mr-10' : 'ml-10'}`}>
+        {item?.img && (
+          <img
+            src={item?.img}
+            alt='wander way'
+            className='w-60 h-44 rounded-md'
+          />
+        )}
       </div>
       <div className='space-y-3'>
         <h1 className='font-bold text-3xl'>{item?.name}</h1>
@@ -20,7 +31,7 @@ const ProjectCard = ({ item, index }) => {
             </div>
           ))}
         </div>
-        <p className='line-clamp-2 max-w-4xl'>{item?.description}</p>
+        <p className=' max-w-4xl'>{item?.description}</p>
       </div>
     </div>
   );
