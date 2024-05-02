d1676fd777fe0b031c2020f6865e767a7ba13a48
ghritak <ghritakjyotikalita@gmail.com>
2024-05-02 12:52:05 +0530

    nav color

--- src/components/nav/NavbarButton.js
+++ src/components/nav/NavbarButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NavbarButton = ({ children }) => {
   return (
-    <div className='cursor-pointer font-semibold text-lg px-5 py-1 rounded-full transition-all duration-300 hover:bg-black hover:text-white'>
+    <div className='cursor-pointer font-semibold text-lg px-5 py-1 rounded-full transition-all duration-300 text-white hover:bg-white hover:text-black'>
       {children}
     </div>
   );
