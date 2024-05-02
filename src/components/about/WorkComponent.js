2514105f3d0115f8175edb5d754a5f6bce626924
ghritak <ghritakjyotikalita@gmail.com>
2024-05-02 13:52:33 +0530

    work exp

--- src/components/about/WorkComponent.js
+++ src/components/about/WorkComponent.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import LeftIndicator from './LeftIndicator';
 import OrgLine from './OrgLine';
+import { work } from './constant';
+import { GoDotFill } from 'react-icons/go';
 
 const WorkComponent = () => {
   return (
@@ -14,35 +16,68 @@ const WorkComponent = () => {
             <div className='flex flex-row space-x-5'>
               <div className=' text-white'>
                 <div className='flex flex-row'>
-                  <LeftIndicator size={140} />
+                  <LeftIndicator size={200} />
                   <div className='space-y-3'>
                     <h1 className='text-4xl font-bold font-serif text-slate-200'>
-                      Bachelor of Technology
+                      Software Developer
                     </h1>
-                    <h1 className='text-2xl font-semibold text-slate-300'>
-                      Electronics and Communication Engineering
-                    </h1>
-                    <OrgLine name={'Tezpur University'} year={'2023'} />
+                    <OrgLine
+                      name={'Cloud Civil Technologies'}
+                      year={'Oct 2023 - Apr 2024'}
+                    />
+                    <div className='space-y-1'>
+                      {work[0].map((item) => (
+                        <div className='flex flex-row items-start'>
+                          <div className='mt-1 mr-1'>
+                            <GoDotFill color='white' size={20} />
+                          </div>
+                          <p className='leading-7'>{item}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
 
                 <div className='flex flex-row'>
-                  <LeftIndicator size={90} />
+                  <LeftIndicator size={170} />
                   <div className='space-y-3'>
-                    <h1 className='text-3xl font-bold font-serif text-slate-200'>
-                      Higher Secondary, AHSEC
+                    <h1 className='text-4xl font-bold font-serif text-slate-200'>
+                      React Native Developer
                     </h1>
-                    <OrgLine name={'Cotton University'} year={'2018'} />
+                    <OrgLine
+                      name={'TGG Technologies Private Limited'}
+                      year={'Sep 2023 - Nov 2023'}
+                    />
+                    <div className='space-y-1'>
+                      {work[1].map((item) => (
+                        <div className='flex flex-row items-start'>
+                          <div className='mt-1 mr-1'>
+                            <GoDotFill color='white' size={20} />
+                          </div>
+                          <p className='leading-7'>{item}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
 
                 <div className='flex flex-row'>
                   <LeftIndicator size={0} />
                   <div className='space-y-3'>
-                    <h1 className='text-3xl font-bold font-serif text-slate-200'>
-                      High School, SEBA
+                    <h1 className='text-4xl font-bold font-serif text-slate-200'>
+                      Software Developement Intern
                     </h1>
-                    <OrgLine name={'Navarun Jatiya Vidyalaya'} year={'2016'} />
+                    <OrgLine name={'AID24x7'} year={'Feb 2023 - Aug 2023'} />
+                    <div className='space-y-1'>
+                      {work[2].map((item) => (
+                        <div className='flex flex-row items-start'>
+                          <div className='mt-1 mr-1'>
+                            <GoDotFill color='white' size={20} />
+                          </div>
+                          <p className='leading-7'>{item}</p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </div>
               </div>
---
+++ src/components/about/constant.js
@@ -0,0 +1,16 @@
+export const work = [
+  [
+    'Developed and launched three responsive front-end applications using React.js and Next.js, alongside two cross-platform mobile applications using React Native and Expo cli. Implemented user-centric features and performance optimizations, resulting in a 20% increase in user engagement.',
+    'Engineered a robust API in Express and Node.js, enabling seamless data retrieval.',
+    'Managed PostgreSQL databases with over 30,000 records, optimizing query performance by 10%.',
+  ],
+  [
+    'Led development of Android and iOS applications, improving UI design and app performance, contributing to a 40% increase in app downloads within the first month of launch.',
+    'Enhanced CS Cart React Native application, resulting in a 25% increase in user satisfaction score.',
+    'Improved app speed and performance by 15% based on user feedback while developing the application.',
+  ],
+  [
+    'Collaborated with a team of three to develop Android and web applications for AID24x7 using React Native.',
+    'Set up Linux-based AWS EC2 instances and integrated Cloudflare CDN, utilizing Nginx as a proxy and PM2 for process management to ensure stable hosting and scalability.',
+  ],
+];
