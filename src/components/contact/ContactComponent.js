03754e80bc9fcd496447c75a55b1028e2702cbc5
ghritak <ghritakjyotikalita@gmail.com>
2024-05-27 14:12:10 +0530

    contact component

--- src/components/contact/ContactComponent.js
+++ src/components/contact/ContactComponent.js
@@ -1,7 +1,22 @@
 import React from 'react';
 
 const ContactComponent = () => {
-  return <div>ContactComponent</div>;
+  return (
+    <section className='mx-56 flex relative px-20'>
+      <div className='flex flex-row min-h-screen space-x-20'>
+        <div className=' flex justify-center items-center'>
+          <div className='space-y-20'>
+            <h1 className='text-6xl font-serif font-bold text-white'>
+              Contact
+            </h1>
+            <div className='flex flex-row space-x-5'>
+              <div className=' text-white space-y-4'></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default ContactComponent;
